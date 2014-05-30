d73b7f03770525c11df5f814de35715187e0c6b7
lorenzofox3 <laurent34azerty@gmail.com>
2014-05-30 10:22:35 +0700

    enhance style

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ var path = require('path');
 exports.server = {
     port: 8000,
     staticFolderUrl: '/static',
-    staticFolderMapping: path.resolve(__dirname, '../dist/'),
+    staticFolderMapping: path.resolve(__dirname, '../public/'),
     cookieSecret: 'a_big_secret',
     dbPath: 'mongodb://localhost:27017/blog-it-dev'
 };
